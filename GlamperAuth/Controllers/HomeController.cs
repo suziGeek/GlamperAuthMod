838150395a94175f127f702a66ab2cc801e0c11c
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-08-11 20:21:46 -0400

    refactored some things

--- GlamperAuth/Controllers/HomeController.cs
+++ GlamperAuth/Controllers/HomeController.cs
@@ -34,5 +34,9 @@ namespace GlamperAuth.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+
     }
 }
