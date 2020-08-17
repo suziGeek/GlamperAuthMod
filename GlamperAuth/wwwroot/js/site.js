9852f42006d5ba5a4d3b81180c0c36739c3432e2
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-08-16 22:01:23 -0400

    adding styling

--- GlamperAuth/wwwroot/js/site.js
+++ GlamperAuth/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function toggle_visibility(list) {
+    var e = document.getElementById(list);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
