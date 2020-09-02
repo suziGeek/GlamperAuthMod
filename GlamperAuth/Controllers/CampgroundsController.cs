d0cf5b8695b4cb397bdebcde61e036f09bca62b7
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-09-02 13:45:45 -0400

    rebuilding campground models and controllers

---
+++ GlamperAuth/Controllers/CampgroundsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GlamperAuthMOD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlamperAuthMOD.Controllers
+{
+    public class CampgroundsController : Controller
+    {
+        private readonly IProductRepository repo;
+
+        public CampgroundsController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index(string selectState)
+        {
+            var client = new HttpClient();
+            var quote = new CampgroundsRepository(client);
+
+            List<Campgrounds> campground = quote.GetCampgrounds(selectState);
+
+            return View(campground);
+        }
+    }
+}
---
+++ GlamperAuth/Models/ICampgroundsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlamperAuthMOD.Models
+{
+    interface ICampgroundsRepository
+    {
+        public List<Campgrounds> GetCampgrounds(string selectState);
+    }
+}
